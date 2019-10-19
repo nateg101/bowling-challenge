e0b4796549dead655f017168dc302ba110ecdc8d
Nat Elliott-Green <natelliottgreen34@gmail.com>
2019-10-19 13:56:21 +0100

    Adds up score from a frame and adds the number of frames.

--- jasmine/spec/BowlingSpec.js
+++ jasmine/spec/BowlingSpec.js
@@ -10,4 +10,19 @@ describe('Bowling', function(){
   it('should start with a score of 0', function(){
     expect(bowling.getCurrentScore()).toEqual(0)
   });
+
+  it('the score is increased after one frame', function(){
+    bowling.throw(2)
+    bowling.throw(4)
+    expect(bowling.getCurrentScore()).toEqual(6)
+  });
+
+  describe('increasing frame number', function(){
+    it('increases the frame number after two throws', function(){
+      bowling.throw(3)
+      bowling.throw(3)
+      expect(bowling.getCurrentFrame()).toEqual(2)
+    });
+  });
+
 })
--- jasmine/src/Bowling.js
+++ jasmine/src/Bowling.js
@@ -3,8 +3,31 @@
 function Bowling(){
   this.START_SCORE = 0
   this._score = this.START_SCORE
+  this._throwOne = false
+  this._throwTwo = false
+  this._frame = 1
 }
 
-Bowling.prototype.getCurrentScore = function() {
+Bowling.prototype.getCurrentScore = function(){
   return this._score;
-};
+}
+
+Bowling.prototype.getCurrentFrame = function(){
+  return this._frame;
+}
+
+Bowling.prototype.throw = function(pins){
+  if(this._throwOne === false) {
+    this._throwOne = pins;
+  } else {
+    this._throwTwo = pins;
+    this.endFrame();
+  };
+}
+
+Bowling.prototype.endFrame = function(){
+  this._frame++
+  this._score += this._throwOne + this._throwTwo
+  this._throwOne = false
+  this._throwTwo = false
+}
